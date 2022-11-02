133286233bc38eaf22ea7f5f63bf81fa5c351a3c
ParkJunHo <junhoyes92@naver.com>
2022-11-02 15:42:47 +0900

    016 숫자형 이해하기

---
+++ part1/016.js
@@ -0,0 +1,7 @@
+//자바스크립트 숫자형:숫자형태의 데이터를 의미
+
+console.log(Infinity); // 값그대로출력
+console.log( 1/ Infinity); //0
+console.log(0 / 0); // 값은0
+console.log(Infinity - Infinity);// 인피니티를 나누면 연산은 NAN
+console.log(0/"뿡");  //유효 하지 않은 수식인경우  연산값 nan
