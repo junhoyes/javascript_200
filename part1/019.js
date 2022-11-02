c0cf61648fa3522753e6304f4e7b7e6c16076438
ParkJunHo <junhoyes92@naver.com>
2022-11-02 16:05:38 +0900

    019 null과 undefind 이해하기

---
+++ part1/019.js
@@ -0,0 +1,9 @@
+//null은 비어있는 존재하지않는 값  undefind은 변수는 정지되있지만 아무것도 할당 받지 못한상태를 의미
+ 
+
+let hi = null; //hi에 null값 할당
+console.log(hi1);//대입된 값 그대로 null출력
+console.log(typeof hi1);//자료형으로 확인 되면 object(객체)로 출력
+let hi2 ; //아무것도 할당x
+console.log(hi2); // 할당된값이 없어서 undefin출력
+
