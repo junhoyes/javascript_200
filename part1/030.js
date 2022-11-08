4bd35b2928266067d85405681ccf3fd110c40db7
ParkJunHo <junhoyes92@naver.com>
2022-11-08 14:38:50 +0900

    030 ES6의 항상된 객체 문법 알아보기 -단축 속성명

---
+++ part1/030.js
@@ -0,0 +1,20 @@
+/*
+es6에 새로 추가된 js기능인 단축 속성명 을 통해 객체의 속성을 좀 더 간단하게 정의가 가능하다.
+{변수명}
+*/
+const adress ='seoul';
+const members = {};
+const addFamily = function(age,name,role){
+    this.members[role] ={age,name};
+}
+const getheadcount = function(){
+    return Object.keys(this,members).length;
+
+    const family = {adress, members,addFamily,getheadcount};
+
+    family.addFamily(30.'chole','aunt');
+    family.addFamily(3.'lyn','niece');
+    family.addFamily(30.'mun','dog');
+    console.log(family.getheadcount());
+
+}
